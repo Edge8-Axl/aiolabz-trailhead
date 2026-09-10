966590a7c6ffa9514d4451ce10ac3f571c71d08c
Axl Joven <axl@edge8.ai>
2026-09-10 10:52:16 +0800

    Fix supabase credentials + revert LayoutProps

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -2,8 +2,7 @@ import { createServerClient } from "@supabase/ssr";
 import { createClient } from "@supabase/supabase-js";
 import { cookies } from "next/headers";
 
-// const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const SUPABASE_URL: number = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 /**
